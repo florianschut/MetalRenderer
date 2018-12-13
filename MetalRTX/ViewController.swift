2d760f64c65d3589449752e49439546ef68689be
Florian Schut <florianschut@mac.com>
2018-12-13 14:58:38 +0100

    [WIP]: First commit of work done while drunk

--- MetalRTX/ViewController.swift
+++ MetalRTX/ViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import Metal
+import MetalKit
 
 class ViewController: UIViewController {
 
+    var device: MTLDevice!
+    var metalLayer: CAMetalLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        device = MTLCreateSystemDefaultDevice()
+        
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        
+        view.layer.addSublayer(metalLayer)
     }
+    
+    
 
 
 }
